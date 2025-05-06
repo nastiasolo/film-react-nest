14e5ff3b4c7d81000d5e061d98fa8451e097834a
nastiasolo <ag.solodova@gmail.com>
2025-05-06 12:04:03 +0200

    tests added

--- backend/src/order/order.service.spec.ts
+++ backend/src/order/order.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Film } from '../entities/film.entity';
 
 describe('OrderService', () => {
   let service: OrderService;
 
+  const mockFilmRepository = {
+    findOne: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderService],
+      providers: [
+        OrderService,
+        {
+          provide: getRepositoryToken(Film),
+          useValue: mockFilmRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<OrderService>(OrderService);
